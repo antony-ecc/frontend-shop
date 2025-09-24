110bc8e31db0694619bdf5d335ec484c4871fe2e
Antony <antony.ecc@gmail.com>
2025-09-24 11:03:54 +0700

    Update endpoint

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://anton.learndev.my.id",
+  baseURL: "https://anton.learndev.my.id/api",
   // baseURL: "http://fp-backend-shop.test/api",
   headers: {
     "Content-Type": "application/json",
